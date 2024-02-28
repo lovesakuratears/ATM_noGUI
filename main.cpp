df5b83d3ffde86aad74c9ce491e4f276ebc5c7a0
Sakura <2392922859@qq.com>
2024-02-28 15:38:33 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int intemp;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Press Enter to continue..." <<std::endl;
+    std::cin >> intemp;
+    std::cout << "You entered: " << intemp << std::endl;
+    return 0;
+}
